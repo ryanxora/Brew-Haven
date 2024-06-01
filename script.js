e02037710489d962ef12559b62e93bfe3c5a51fa
ryan <imalkedd2@gmail.com>
2024-06-01 12:02:57 +0530

    testing mobile desktop site bug

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ document.querySelectorAll(".nav-item").forEach(n => n.
         hamburger.classList.remove("active");
         navMenu.classList.remove("active");
     })) 
+
+
+    
+// minn height problem
+
+function adjustForDesktopMode() {
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    const isDesktopMode = window.innerWidth > 768 && isMobile;
+
+    if (isDesktopMode) {
+      document.body.style.minHeight = 'auto'; // Adjust this as needed
+    } else {
+      document.body.style.minHeight = '100vh';
+    }
+  }
+
+  window.addEventListener('resize', adjustForDesktopMode);
+  window.addEventListener('load', adjustForDesktopMode);
+
